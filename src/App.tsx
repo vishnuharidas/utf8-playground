400bb93ce08f5c4ac9a08fc2eb254b7b233c91d0
Vishnu Haridas <hvishnu999@gmail.com>
2025-04-24 17:58:34 +0400

    Improve root layout - rearrange and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,31 @@ function App() {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="w-screen bg-gray-200 p-8 border-amber-500 border-2">
+
+      { /* Header */}
+      <div className="w-full box-border p-4 bg-gray-200">
         <h1 className="text-3xl font-bold">UTF-8 Playground</h1>
+        <p className="text-gray-600">
+          This is a playground for exploring UTF-8 encoding.
+          UTF-8 encoding uses 4 bytes to represent Unicode characters. Read more about UTF-8 <a href="https://en.wikipedia.org/wiki/UTF-8#Description" className="text-blue-500 underline" target='_blank'>here</a>.
+        </p>
       </div>
-      <div className="flex flex-1 border-yellow-500 border-2 p-3">
+
+      { /* Main Content */}
+      <div className="w-full flex flex-1 flex-column min-h-[60vh] border-yellow-500 border-2 p-3">
 
         <CodePointDisplay
-          className="w-8/10 bg-gray-100 p-4 border-blue-500 border-2 container flex-col flex"
+          className="flex-grow bg-gray-100 p-4 border-blue-500 border-2 container flex-col flex"
           codePoint={bitwiseValue} />
 
-        <NotesPanel className="w-2/10 bg-gray-200 p-8 border-red-500 border-2" />
+        <NotesPanel className="w-2/5 bg-gray-200 p-8" />
       </div>
 
-      <BitwiseControl className="p-8 border-2 border-red-500 flex-col flex" value={bitwiseValue} onChange={setBitwiseValue} />
+      { /* Footer */}
+      <footer className="w-full bg-gray-100 p-8 border-2 border-gray-300">
+        <BitwiseControl className="bg-gray-100" value={bitwiseValue} onChange={setBitwiseValue} />
+      </footer>
+
     </div>
   );
 }
