db197cb5ca7294eec8029f38a57f5a4405eea892
Vishnu Haridas <hvishnu999@gmail.com>
2025-04-25 23:20:22 +0400

    Move header to a new component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import BitwiseControl from './components/BitwiseControl';
 import CodePointDisplay from './components/CodePointDisplay';
+import Header from './components/Header';
 import NotesPanel from './components/NotesPanel';
 import { getRandomUtf8 } from './utf8/utf8';
 
@@ -28,13 +29,9 @@ function App() {
     <div className="flex flex-col h-screen">
 
       { /* Header */}
-      <div className="w-full box-border p-4 bg-gray-200">
-        <h1 className="text-3xl font-bold">UTF-8 Playground</h1>
-        <p className="text-gray-600">
-          This is a playground for exploring UTF-8 encoding.
-          UTF-8 encoding uses 4 bytes to represent Unicode characters. Read more about UTF-8 <a href="https://en.wikipedia.org/wiki/UTF-8#Description" className="text-blue-500 underline" target='_blank'>here</a>.
-        </p>
-      </div>
+      <header className="w-full bg-gray-200">
+        <Header />
+      </header>
 
       { /* Main Content */}
       <div className="w-full flex flex-1 flex-column min-h-[60vh] border-yellow-500 border-">
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function AppHeader() {
+    return (
+        <div className="w-full box-border p-4 bg-gray-200">
+            <h1 className="text-3xl font-extrabold text-gray-800">UTF-8 Playground</h1>
+            <p className="mt-2 text-[12pt] text-gray-700">
+                Explore and understand UTF-8 encoding, the most popular Unicode character encoding that uses up to 4 bytes to represent Unicode characters.
+            </p>
+        </div>
+    );
+}
+
+export default AppHeader;
